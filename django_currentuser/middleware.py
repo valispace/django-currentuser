e4cc4dcc1a065baaa88d2a1f894e33875310a7fd
antoniolopesvs <114222555+antoniolopesvs@users.noreply.github.com>
2023-03-22 14:36:45 +0000

    Rollback code to the previous version
    Current code do not work with django4 in django unit tests. It is always creating new threads which makes the local() loses the previous saved values

--- django_currentuser/middleware.py
+++ django_currentuser/middleware.py
@@ -21,17 +21,6 @@ def _set_current_user(user=None):
     _do_set_current_user(lambda self: user)
 
 
-class SetCurrentUser:
-    def __init__(this, request):
-        this.request = request
-
-    def __enter__(this):
-        _do_set_current_user(lambda self: getattr(this.request, 'user', None))
-
-    def __exit__(this, type, value, traceback):
-        _do_set_current_user(lambda self: None)
-
-
 class ThreadLocalUserMiddleware(object):
 
     def __init__(self, get_response):
@@ -41,8 +30,7 @@ class ThreadLocalUserMiddleware(object):
         # request.user closure; asserts laziness;
         # memorization is implemented in
         # request.user (non-data descriptor)
-        with SetCurrentUser(request):
-            response = self.get_response(request)
+        response = self.get_response(request)
         return response
 
 
